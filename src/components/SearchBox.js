1cbaf92c5db89d3b72e44ec2defc0bcc349e901c
shanghanrun <idim7@naver.com>
2024-05-04 14:52:15 +0900

    .

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import { useSearchParams } from "react-router-dom";
+// import { useSearchParams } from "react-router-dom";
 
 const SearchBox = ({ searchQuery, setSearchQuery, placeholder, field }) => {
-  const [query] = useSearchParams();
-  const [keyword, setKeyword] = useState(query.get(field) || "");
+  // const [query] = useSearchParams();
+  // const [keyword, setKeyword] = useState(query.get(field) || "");
+  const [keyword, setKeyword] = useState('')
 
   const onCheckEnter = (event) => {
     if (event.key === "Enter") {
